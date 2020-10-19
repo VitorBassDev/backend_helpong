85606f2af91f5bc96a1867da5cc15d305a285910
Vitor Guedes <vittorbassdev@gmail.com>
2020-10-18 22:30:10 -0300

    Implementação Banco de dados userController - Buscar e Inserir ONG

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,6 +53,45 @@ module.exports = {
     });
   },
 
+  async create_user_doador (request, response) {
+    const  {
+      nome_usuario,
+      email_usuario,
+      senha_usuario,
+    } = request.body;
+
+    const dados = request.body;    
+
+    await bcrypt.hash(request.body.senha_usuario, 10,(errBcrypt, hash) => {
+      if(errBcrypt){
+        return res.status(500).json({
+          error: errBcrypt,
+          Mensagem: "Erro na Senha"
+        });
+      } else {
+        connection.insert({
+          nome_usuario, 
+          email_usuario,
+          senha_usuario: hash,
+          perfil_usuario: 2
+          }).into('tbl_usuario').then(data =>{
+            return response.json({
+              mensagem: 'Usuario Cadastrado com Sucesso',
+                usuarioCriado: {
+                Nome_Usuário: request.body.nome_usuario,    
+                //Senha_Usuário: hash,    
+                }
+              });
+            }).catch(err => {
+                console.log(err);
+                return response.json({
+                  Mensagem: "Erro no Cadastro"
+                });
+              });
+        }
+    });
+  },
+
   create_user_test (request, response) {
     const resposta =  [request.body.nome, request.body.email, request.body.senha]
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -7,6 +7,7 @@ const UserController = require('../controllers/userController');
 
 router.get('/user-test', UserController.select_user_test);
 router.post('/user-ong', UserController.create_user_ong);
+router.post('/user-doador', UserController.create_user_doador);
 router.post('/user-ong', UserController.create_user_test);
 
 module.exports = router;
