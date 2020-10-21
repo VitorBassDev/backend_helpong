82bc7422a61d800a4bb7c218cc0b945ca29bf6ae
Vitor Guedes <vittorbassdev@gmail.com>
2020-10-21 13:59:20 -0300

    Implementação Buscar Email passado no body e o perfil de usuário no banco de dados

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,32 @@
-const bcrypt = require ('bcrypt');
-module.exports = {
-  auth_ong (request, response) {
-  const resposta =  {
-    mensagem : "Get_User - Cheguei Aqui"
-  }
-    console.log(resposta) 
+const connection  = require ('../database/connection');
 
-  return response.json(resposta);
+module.exports = {
+
+  async auth_ong (request, response, next){
+
+    const  {
+      email_usuario,
+      //senha_usuario
+    } = request.body;
+  
+    // BUSCAR EMAIL E TIPO DE PERFIL NO BANCO DE DADOS
+    const ong = await connection('tbl_usuario')
+      .where('email_usuario', email_usuario)
+      .whereRaw('perfil_usuario = ?', 1)
+      .select('nome_usuario', 'perfil_usuario')
+      .first();
+      // VALIDAR O RESULTADO DA QUERY ACIMA
+      if(!ong){
+        console.log("Email Não encontrado" )
+        return response.status(400).json({ error: 'No Ong Found with this id'});
+      } else {
+        // MOSTRAR O RESULTADO DA QUERY, CASO FOR VERDADEIRA
+        console.log(ong)
+        return response.json(ong);
+      }
+      // MOSTRAR O RESULTADO DA QUERY, CASO FOR VERDADEIRA
+      // console.log(ong)
+      // return response.json(ong);
   },
 
   auth_doador (request, response) {
