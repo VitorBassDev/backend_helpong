a9963ffee73e10697f4149241ea4d217ab1cb31c
Vitor Guedes <vittorbassdev@gmail.com>
2020-10-21 21:59:10 -0300

    Implementaçaõo Validação de usário com perfil ong Validação de email, senha e ID

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,5 @@
+const bcrypt = require ('bcrypt');
+const jwt  = require ('jsonwebtoken');
 const connection  = require ('../database/connection');
 
 module.exports = {
@@ -6,27 +8,42 @@ module.exports = {
 
     const  {
       email_usuario,
-      //senha_usuario
+      senha_usuario
     } = request.body;
+
+    var senha = request.body.senha_usuario;
   
     // BUSCAR EMAIL E TIPO DE PERFIL NO BANCO DE DADOS
-    const ong = await connection('tbl_usuario')
+    const user = await connection('tbl_usuario')
       .where('email_usuario', email_usuario)
+      //.whereRaw('senha_usuario', senha_usuario )
       .whereRaw('perfil_usuario = ?', 1)
-      .select('nome_usuario', 'perfil_usuario')
+      .select('nome_usuario', 'perfil_usuario', 'senha_usuario')
       .first();
-      // VALIDAR O RESULTADO DA QUERY ACIMA
-      if(!ong){
+       // VALIDAR O RESULTADO DA QUERY ACIMA
+      if(!user){
         console.log("Email Não encontrado" )
-        return response.status(400).json({ error: 'No Ong Found with this id'});
+        return response.status(400).json({ error: 'No Email Ong Found with this id'});
       } else {
-        // MOSTRAR O RESULTADO DA QUERY, CASO FOR VERDADEIRA
-        console.log(ong)
-        return response.json(ong);
-      }
-      // MOSTRAR O RESULTADO DA QUERY, CASO FOR VERDADEIRA
-      // console.log(ong)
-      // return response.json(ong);
+          const match = await bcrypt.compare(senha, user.senha_usuario);
+            if(match) {
+              console.log('Granted! Senha Verificada');
+              const token = jwt.sign({
+                id: user.email_usuario
+              }, "segredo", {
+                expiresIn: "1h"
+                });
+                  return response.status(200).json({
+                    Mensagem: "Autenticado com Sucesso",
+                    token, user
+                  });
+            } else {
+              console.log('Access Denied');
+              return response.status(400).json({
+                error: 'Acesso Negado'
+              })
+            }          
+          }
   },
 
   auth_doador (request, response) {
