7cbd09b75c8a0148f86d8d0c6f4d129afd0e38cf
Vitor Guedes <vittorbassdev@gmail.com>
2020-10-22 08:09:08 -0300

    Implementação Usuário Doador Validação de email e senha

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,12 +46,45 @@ module.exports = {
           }
   },
 
-  auth_doador (request, response) {
-    const resposta =  {
-      mensagem : "Get_User - Cheguei Aqui"
-    }
-      console.log(resposta) 
+  async auth_doador (request, response, next){
+
+    const  {
+      email_usuario,
+      senha_usuario
+    } = request.body;
+
+    var senha = request.body.senha_usuario;
   
-    return response.json(resposta);
+    // BUSCAR EMAIL E TIPO DE PERFIL NO BANCO DE DADOS
+    const user = await connection('tbl_usuario')
+      .where('email_usuario', email_usuario)
+      //.whereRaw('senha_usuario', senha_usuario )
+      .whereRaw('perfil_usuario = ?', 2)
+      .select('nome_usuario', 'perfil_usuario', 'senha_usuario')
+      .first();
+       // VALIDAR O RESULTADO DA QUERY ACIMA
+      if(!user){
+        console.log("Email Não encontrado" )
+        return response.status(400).json({ error: 'No Email Ong Found with this id'});
+      } else {
+          const match = await bcrypt.compare(senha, user.senha_usuario);
+            if(match) {
+              console.log('Granted! Senha Verificada');
+              const token = jwt.sign({
+                id: user.email_usuario
+              }, "segredo", {
+                expiresIn: "1h"
+                });
+                  return response.status(200).json({
+                    Mensagem: "Autenticado com Sucesso",
+                    token, user
+                  });
+            } else {
+              console.log('Access Denied');
+              return response.status(400).json({
+                error: 'Acesso Negado'
+              })
+            }          
+          }
   },
 }
